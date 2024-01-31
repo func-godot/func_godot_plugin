7298993d931c16971246791074ae35558fe3d9f0
RhapsodyInGeek <44485952+RhapsodyInGeek@users.noreply.github.com>
2024-01-30 20:35:14 -0500

    Update Property Parsing in FGD Entity Class
    - Removed cyclic reference to FuncGodotFGDSolidClass and instead check for the "@SolidClass" prefix.
    - Changed "if else" chain into a match statement.
    - Added support for Bool, Vector2, Vector2i, Vector3i, Vector4, Vector4i.

--- addons/func_godot/src/fgd/func_godot_fgd_entity_class.gd
+++ addons/func_godot/src/fgd/func_godot_fgd_entity_class.gd
@@ -56,7 +56,7 @@ func build_def_text() -> String:
 		meta_props['base'] = base_str
 
 	for prop in meta_props:
-		if self is FuncGodotFGDSolidClass:
+		if prefix == '@SolidClass':
 			if prop == "size" or prop == "model":
 				continue
 		
@@ -111,49 +111,54 @@ func build_def_text() -> String:
 				prop_description = "\"" + class_property_descriptions[prop] + "\""
 		else:
 			prop_description = "\"\""
+		
+		match typeof(value):
+			TYPE_INT:
+				prop_type = "integer"
+				prop_val = str(value)
+			TYPE_FLOAT:
+				prop_type = "float"
+				prop_val = "\"" + str(value) + "\""
+			TYPE_STRING:
+				prop_type = "string"
+				prop_val = "\"" + value + "\""
+			TYPE_BOOL:
+				prop_type = "choices"
+				prop_val = "[" + "\n"
+				prop_val += "\t\t" + str(0) + " : \"False\"\n"
+				prop_val += "\t\t" + str(1) + " : \"True\"\n"
+				prop_val += "\t]"
+			TYPE_VECTOR2, TYPE_VECTOR2I:
+				prop_type = "string"
+				prop_val = "\"%s %s\"" % [value.x, value.y]
+			TYPE_VECTOR3, TYPE_VECTOR3I:
+				prop_type = "string"
+				prop_val = "\"%s %s %s\"" % [value.x, value.y, value.z]
+			TYPE_VECTOR4, TYPE_VECTOR4I:
+				prop_type = "string"
+				prop_val = "\"%s %s %s %s\"" % [value[0], value[1], value[2], value[3]]
+			TYPE_COLOR:
+				prop_type = "color255"
+				prop_val = "\"%s %s %s\"" % [value.r8, value.g8, value.b8]
+			TYPE_DICTIONARY:
+				prop_type = "choices"
+				prop_val = "[" + "\n"
+				for choice in value:
+					var choice_val = value[choice]
+					prop_val += "\t\t" + str(choice_val) + " : \"" + choice + "\"\n"
+				prop_val += "\t]"
+			TYPE_ARRAY:
+				prop_type = "flags"
+				prop_val = "[" + "\n"
+				for arr_val in value:
+					prop_val += "\t\t" + str(arr_val[1]) + " : \"" + str(arr_val[0]) + "\" : " + ("1" if arr_val[2] else "0") + "\n"
+				prop_val += "\t]"
+			TYPE_NODE_PATH:
+				prop_type = "target_destination"
+			TYPE_OBJECT:
+				prop_type = "target_source"
 
-		if value is int:
-			prop_type = "integer"
-			prop_val = str(value)
-		elif value is float:
-			prop_type = "float"
-			prop_val = "\"" + str(value) + "\""
-		elif value is String:
-			prop_type = "string"
-			prop_val = "\"" + value + "\""
-		elif value is Vector3:
-			prop_type = "string"
-			prop_val = "\"%s %s %s\"" % [
-				value.x,
-				value.y,
-				value.z
-			]
-		elif value is Color:
-			prop_type = "color255"
-			prop_val = "\"%s %s %s\"" % [
-				value.r8,
-				value.g8,
-				value.b8
-			]
-		elif value is Dictionary:
-			prop_type = "choices"
-			prop_val = "[" + "\n"
-			for choice in value:
-				var choice_val = value[choice]
-				prop_val += "\t\t" + str(choice_val) + " : \"" + choice + "\"\n"
-			prop_val += "\t]"
-		elif value is Array:
-			prop_type = "flags"
-			prop_val = "[" + "\n"
-			for arr_val in value:
-				prop_val += "\t\t" + str(arr_val[1]) + " : \"" + str(arr_val[0]) + "\" : " + ("1" if arr_val[2] else "0") + "\n"
-			prop_val += "\t]"
-		elif value is NodePath:
-			prop_type = "target_destination"
-		elif value is Object:
-			prop_type = "target_source"
-
-		if(prop_val):
+		if prop_val:
 			res += "\t"
 			res += prop
 			res += "("
@@ -165,7 +170,7 @@ func build_def_text() -> String:
 					res += " : "
 					res += prop_description
 
-			if value is Dictionary or value is Array:
+			if value is bool or value is Dictionary or value is Array:
 				res += " = "
 			else:
 				res += " : "
