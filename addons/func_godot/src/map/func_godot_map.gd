55f4af7b62acb84ade15020c7cc716c805c35308
Pspritechologist <81725545+Pspritechologist@users.noreply.github.com>
2024-05-05 00:20:15 -0400

    Switched description and name

--- addons/func_godot/src/map/func_godot_map.gd
+++ addons/func_godot/src/map/func_godot_map.gd
@@ -40,10 +40,8 @@ var _map_file_internal: String = ""
 @export var block_until_complete: bool = false
 ## How many nodes to set the owner of, or add children of, at once. Higher values may lead to quicker build times, but a less responsive editor.
 @export var set_owner_batch_size: int = 1000
-## If true, automatically triggers a build on this Node when the relevant local map file is imported.
-## An import can occur when the Resource is first added, when the resource is updated externally, or when an import is manually triggered.
-## This means saving over the map file and tabbing into Godot will cause the map to update automatically.
-@export var auto_build_on_local_file_import: bool = true
+## If true, automatically rebuilds the map when the local map file is reimported after being saved externally.
+@export var auto_build_on_local_map_update: bool = true
 
 # Build context variables
 var func_godot: FuncGodot = null
@@ -73,7 +71,7 @@ var entity_collision_shapes: Array = []
 func _ready() -> void:
 	if Engine.is_editor_hint():
 		EditorInterface.get_resource_filesystem().resources_reimported.connect(_on_reimport)
-		if auto_build_on_local_file_import:
+		if auto_build_on_local_map_update:
 			verify_and_build()
 	
 	if not DEBUG:
@@ -92,7 +90,7 @@ func verify_and_build() -> void:
 		emit_signal("build_failed")
 
 func _on_reimport(resources: PackedStringArray) -> void:
-	if !auto_build_on_local_file_import:
+	if !auto_build_on_local_map_update:
 		return
 	
 	for resource in resources:
