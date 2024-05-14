8c195e0d8dcf9a3cdfb4c52c03c9a1f36dff0edb
Pspritechologist <81725545+Pspritechologist@users.noreply.github.com>
2024-05-14 03:02:30 -0400

    Whoops :P

--- addons/func_godot/src/func_godot_plugin.gd
+++ addons/func_godot/src/func_godot_plugin.gd
@@ -76,11 +76,11 @@ func _exit_tree() -> void:
 		func_godot_map_progress_bar = null
 	
 	# Cleanup Editor Settings
-	FuncGodotLocalConfig.remove_editor_settings()
+	#INFO: I realized that doing this defeats the purpose of using EditorSettings
+	# FuncGodotLocalConfig.remove_editor_settings()
 	
 	# Cleanup Project Settings
-	#INFO: I realized that doing this defeats the purpose of using EditorSettings
-	# FuncGodotProjectConfig.remove_project_settings()
+	FuncGodotProjectConfig.remove_project_settings()
 
 ## Create the toolbar controls for [FuncGodotMap] instances in the editor
 func create_func_godot_map_control() -> Control:
