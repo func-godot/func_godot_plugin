81372c51ef05c27bccaaa87005dbdf6967073bc2
RhapsodyInGeek <44485952+RhapsodyInGeek@users.noreply.github.com>
2025-08-30 18:01:57 -0400

    Fix Model Point Class Generate Size Property AABB Calculations
    Auto generating the `size` meta property did not calculate from the Project Settings' `default_inverse_scale_factor`. Additionally, the AABB did not get itself positioned according to the model position. This PR fixes both issues. AABBs now more accurately reflect the actual mesh bounds (though still not perfectly).
    
    Resolves #152 .

--- addons/func_godot/src/fgd/func_godot_fgd_model_point_class.gd
+++ addons/func_godot/src/fgd/func_godot_fgd_model_point_class.gd
@@ -53,13 +53,15 @@ func _generate_model() -> void:
 		return 
 	
 	var gltf_state := GLTFState.new()
-	var path = _get_export_dir()
-	var node = _get_node()
-	if node == null: return
+	var path: String = _get_export_dir()
+	var node: Node3D = _get_node()
+	if not node:
+		return
 	if not _create_gltf_file(gltf_state, path, node):
 		printerr("could not create gltf file")
 		return
 	node.queue_free()
+	
 	if target_map_editor == TargetMapEditor.TRENCHBROOM:
 		const model_key: String = "model"
 		if scale_expression.is_empty():
@@ -73,7 +75,7 @@ func _generate_model() -> void:
 		meta_properties["studio"] = '"%s"' % _get_local_path()
 	
 	if generate_size_property:
-		meta_properties["size"] = _generate_size_from_aabb(gltf_state.meshes)
+		meta_properties["size"] = _generate_size_from_aabb(gltf_state.meshes, gltf_state.get_nodes())
 
 func _get_node() -> Node3D:
 	var node := scene_file.instantiate()
@@ -145,26 +147,38 @@ func _save_to_file_system(gltf_document: GLTFDocument, gltf_state: GLTFState, pa
 		return 
 	print('Exported model to ', path)
 
-func _generate_size_from_aabb(meshes: Array[GLTFMesh]) -> AABB:
+func _generate_size_from_aabb(meshes: Array[GLTFMesh], nodes: Array[GLTFNode]) -> AABB:
 	var aabb := AABB()
 	for mesh in meshes:
 		aabb = aabb.merge(mesh.mesh.get_mesh().get_aabb())
-
+	var pos_ofs := Vector3.ZERO
+	if not nodes.is_empty():
+		var ct: int = 0
+		for node in nodes:
+			if node.parent == 0:
+				pos_ofs += node.position
+				ct += 1
+		pos_ofs /= maxi(ct, 1)
+	aabb.position += pos_ofs
+	
 	# Reorient the AABB so it matches TrenchBroom's coordinate system
 	var size_prop := AABB()
 	size_prop.position = Vector3(aabb.position.z, aabb.position.x, aabb.position.y)
 	size_prop.size = Vector3(aabb.size.z, aabb.size.x, aabb.size.y)
-
+	
 	# Scale the size bounds to our scale factor
 	# Scale factor will need to be set if we decide to auto-generate our bounds
 	var scale_factor: Vector3 = Vector3.ONE
 	if target_map_editor == TargetMapEditor.TRENCHBROOM:
-		if scale_expression.begins_with('\''):
-			var scale_arr := scale_expression.split_floats(' ', false)
-			if scale_arr.size() == 3:
-				scale_factor *= Vector3(scale_arr[0], scale_arr[1], scale_arr[2])
-		elif scale_expression.to_float() > 0:
-			scale_factor *= scale_expression.to_float()
+		if scale_expression.is_empty():
+			scale_factor *= ProjectSettings.get_setting("func_godot/default_inverse_scale_factor", 32.0) as float
+		else:
+			if scale_expression.begins_with('\''):
+				var scale_arr := scale_expression.split_floats(' ', false)
+				if scale_arr.size() == 3:
+					scale_factor *= Vector3(scale_arr[0], scale_arr[1], scale_arr[2])
+			elif scale_expression.to_float() > 0:
+				scale_factor *= scale_expression.to_float()
 	
 	size_prop.position *= scale_factor
 	size_prop.size *= scale_factor
