cdf27e3cd8369b405a0ac48649e141238f5cfad5
Sam Bloomberg <sam@xbloom.io>
2026-09-07 21:15:12 -0400

    Apply an entity's class properties after its base class' (#275)
    This fixes an issue where a child entity class couldn't override the a base class' default value for a property.

--- addons/func_godot/src/fgd/func_godot_fgd_entity_class.gd
+++ addons/func_godot/src/fgd/func_godot_fgd_entity_class.gd
@@ -246,15 +246,15 @@ func build_def_text(target_editor: FuncGodotFGDFile.FuncGodotTargetMapEditors = 
 	return res
 
 func retrieve_all_class_properties(properties: Dictionary[String, Variant] = {}) -> Dictionary[String, Variant]:
-	for key in class_properties.keys():
-		properties[key] = class_properties[key]
 	for b in base_classes:
 		properties = b.retrieve_all_class_properties(properties)
+	for key in class_properties.keys():
+		properties[key] = class_properties[key]
 	return properties
 
 func retrieve_all_class_property_descriptions(descriptions: Dictionary[String, Variant] = {}) -> Dictionary[String, Variant]:
-	for key in class_property_descriptions.keys():
-		descriptions[key] = class_property_descriptions[key]
 	for b in base_classes:
 		descriptions = b.retrieve_all_class_property_descriptions(descriptions)
+	for key in class_property_descriptions.keys():
+		descriptions[key] = class_property_descriptions[key]
 	return descriptions
